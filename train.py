2c4b1fcd0310e49c832f1a5677c6ca47bf6d10f9
p350 <442391656@qq.com>
2022-09-27 19:46:45 +0800

    重装Ubuntu 22.04后把代码重新跑起来了

--- train.py
+++ train.py
@@ -24,12 +24,12 @@ def main(args=None):
     parser = argparse.ArgumentParser(description='Simple training script for training a RetinaNet network.')
 
     parser.add_argument('--dataset', default='coco', help='Dataset type, must be one of csv or coco.')
-    parser.add_argument('--coco_path', default='/home/lenovo/lfc/DataSet/coco', help='Path to COCO directory')
+    parser.add_argument('--coco_path', default='/home/p350/lfc/dataset/coco', help='Path to COCO directory')
     parser.add_argument('--csv_train', help='Path to file containing training annotations (see readme)')
     parser.add_argument('--csv_classes', help='Path to file containing class list (see readme)')
     parser.add_argument('--csv_val', help='Path to file containing validation annotations (optional, see readme)')
 
-    parser.add_argument('--depth', help='Resnet depth, must be one of 18, 34, 50, 101, 152', type=int, default=152)
+    parser.add_argument('--depth', help='Resnet depth, must be one of 18, 34, 50, 101, 152', type=int, default=50)
     parser.add_argument('--epochs', help='Number of epochs', type=int, default=2)
 
     parser = parser.parse_args(args)
@@ -67,11 +67,11 @@ def main(args=None):
     else:
         raise ValueError('Dataset type not understood (must be csv or coco), exiting.')
 
-    sampler = AspectRatioBasedSampler(dataset_train, batch_size=6, drop_last=False)
+    sampler = AspectRatioBasedSampler(dataset_train, batch_size=8, drop_last=False)
     dataloader_train = DataLoader(dataset_train, num_workers=16, collate_fn=collater, batch_sampler=sampler)
 
     if dataset_val is not None:
-        sampler_val = AspectRatioBasedSampler(dataset_val, batch_size=6, drop_last=False)
+        sampler_val = AspectRatioBasedSampler(dataset_val, batch_size=8, drop_last=False)
         dataloader_val = DataLoader(dataset_val, num_workers=16, collate_fn=collater, batch_sampler=sampler_val)
 
     # Create the model
